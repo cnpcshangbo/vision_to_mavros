e84700efc3ace84f2bc975c09cb5e33011c5fcc3
Bo Shang <cnpcshangbo@gmail.com>
2020-11-05 09:29:27 -0600

    Edited port for MavProxy.

--- scripts/t265.sh
+++ scripts/t265.sh
@@ -3,4 +3,5 @@
 # # do all your commands here... script terminates when all is done. 
 
 # Change the path to t265_to_mavlink.py
-/home/ubuntu/catkin_ws/src/vision_to_mavros/scripts/t265_to_mavlink.py
+#/home/ubuntu/catkin_ws/src/vision_to_mavros/scripts/t265_to_mavlink.py
+python2 /home/nvidia/catkin_ws/src/vision_to_mavros/scripts/t265_to_mavlink.py
--- scripts/t265_to_mavlink.py
+++ scripts/t265_to_mavlink.py
@@ -37,8 +37,9 @@ from pymavlink import mavutil
 #######################################
 
 # Default configurations for connection to the FCU
-connection_string_default = '/dev/ttyTHS2'
+#connection_string_default = '/dev/ttyTHS2'
 connection_baudrate_default = 921600
+connection_string_default = '127.0.0.1:14550' 
 connection_timeout_sec_default = 5
 
 # Transformation to convert different camera orientations to NED convention. Replace camera_orientation_default for your configuration.
@@ -286,6 +287,9 @@ def send_vision_position_delta_message():
                 current_confidence_level # Normalized confidence value from 0 to 100. 
             )
             vehicle.send_mavlink(msg)
+            # Printing value of now.  
+            print ("Sent mavlink msg at: ")  
+            print (current_time_us)  # Printing value of now.  
             vehicle.flush()
 
             # Save static variables
@@ -498,18 +502,18 @@ if compass_enabled == 1:
 sched = BackgroundScheduler()
 
 if enable_msg_vision_position_estimate:
-    sched.add_job(send_vision_position_estimate_message, 'interval', seconds = 1/vision_position_estimate_msg_hz)
+    sched.add_job(send_vision_position_estimate_message, 'interval', seconds = 1./vision_position_estimate_msg_hz)
 
 if enable_msg_vision_position_delta:
-    sched.add_job(send_vision_position_delta_message, 'interval', seconds = 1/vision_position_delta_msg_hz)
+    sched.add_job(send_vision_position_delta_message, 'interval', seconds = 1./vision_position_delta_msg_hz)
     send_vision_position_delta_message.H_aeroRef_PrevAeroBody = tf.quaternion_matrix([1,0,0,0]) 
     send_vision_position_delta_message.prev_time_us = int(round(time.time() * 1000000))
 
 if enable_msg_vision_speed_estimate:
-    sched.add_job(send_vision_speed_estimate_message, 'interval', seconds = 1/vision_speed_estimate_msg_hz)
+    sched.add_job(send_vision_speed_estimate_message, 'interval', seconds = 1./vision_speed_estimate_msg_hz)
 
 if enable_update_tracking_confidence_to_gcs:
-    sched.add_job(update_tracking_confidence_to_gcs, 'interval', seconds = 1/update_tracking_confidence_to_gcs_hz_default)
+    sched.add_job(update_tracking_confidence_to_gcs, 'interval', seconds = 1./update_tracking_confidence_to_gcs_hz_default)
     update_tracking_confidence_to_gcs.prev_confidence_level = -1
 
 # A separate thread to monitor user input
